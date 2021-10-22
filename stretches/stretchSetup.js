041e9891eb698b17ea4f2b1388178a7cd6ef6779
PlasmaDiffusion <Scott50000@gmail.com>
2021-10-22 10:57:34 -0400

    Refactor half of code into stretchSetup + sObj

---
+++ stretches/stretchSetup.js
@@ -0,0 +1,121 @@
+
+const stretchSet = {
+    CPPS: 0,
+    FAB_SEVEN: 1
+  }
+  
+
+//Change to a different set of stretches depending on url query
+export function changeStretchSet(sObj)
+{
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  sObj.set = urlParams.get('q');
+
+  if (sObj.set == null || sObj.set == sObj.stretchSet.CPPS)
+  {
+    sObj.stretchTimeMultipliers = [1, 1, 1, 2, 2, 1, 1, 1, 2];
+    sObj.stretchNames = [
+    "Squat Stretch",
+    "Leg Push",
+    "Calves",
+    "Hamstring Tie",
+    "Figure Four",
+    "Abdomen",
+    "Child Pose",
+    "Butterfly",
+    "Forward Hip",
+  ];
+  
+  sObj.secondsPerStretch = getCookie("timeForSet0");
+  if (sObj.secondsPerStretch == "") sObj.secondsPerStretch = 120;
+
+  }
+  else if (sObj.set == stretchSet.FAB_SEVEN)
+  {
+    sObj.stretchTimeMultipliers = [
+      2,
+      1.5,
+      1.5,
+      1,
+      1,
+      1,
+      2,
+      1,
+      1,
+    2];
+    sObj.stretchNames = [
+    "Oburator",
+    "Lunge Set 1",
+    "Lunge Set 2",
+    "Child Pose",
+    "Cat Stretch",
+    "Chest Stretch",
+    "Hamstrings",
+    "Bridging",
+    "Tabletop",
+    "Rev. Kegel"
+
+  ];
+  
+  sObj.secondsPerStretch = getCookie("timeForSet1");
+  if (sObj.secondsPerStretch == "") sObj.secondsPerStretch = 120;
+
+  }
+
+  loadStretchMultipliers(sObj);
+
+  document.getElementById("halveTime").innerHTML = "Time " + sObj.secondsPerStretch;
+
+}
+
+export function createCookie(name, value, days) {
+  var date, expires;
+  if (days) {
+      date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      expires = "; expires="+date.toUTCString();
+  } else {
+      expires = "";
+  }
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+
+
+export function getCookie(cname) {
+  //console.log(document.cookie);
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+export function saveStretchMultipliers(sObj)
+{
+  for (let i =0; i < sObj.stretchNames.length; i++)
+  {
+    createCookie(sObj.set+"multiplier"+i, sObj.stretchTimeMultipliers[i], 30);
+    createCookie(sObj.set+"enabled"+i, sObj.stretchEnabled[i], true);
+  }
+}
+
+export function loadStretchMultipliers(sObj)
+{
+  for (let i =0; i < sObj.stretchNames.length; i++)
+  {
+    sObj.stretchTimeMultipliers[i] = getCookie(sObj.set+"multiplier"+i);
+    sObj.stretchEnabled.push(getCookie(sObj.set+"enabled"+i));
+  }
+}
--- stretches/stretches.js
+++ stretches/stretches.js
@@ -1,71 +1,88 @@
+import { loadStretchMultipliers,
+  saveStretchMultipliers,
+  changeStretchSet,
+  createCookie,
+  getCookie
+} from "./stretchSetup.js";
+
 //Global timer
 var time = 0;
 var timer = setInterval(Tick, 1000);
+
 
 const stretchSet = {
   CPPS: 0,
   FAB_SEVEN: 1
 }
 
-var set = stretchSet.CPPS;
 
-
-var stretchNames = [
-  "Squat Stretch",
-  "Leg Push",
-  "Calves",
-  "Hamstring Tie",
-  "Figure Four",
-  "Abdomen",
-  "Child Pose",
-  "Butterfly",
-  "Forward Hip",
-];
-var currentStretch = -1; //Starts at negative to skip the first one if disabled
-//Debug stretches
-/*var stretchTimeMultipliers = [
-  0.016,
-  0.016,
-  0.036,
-  0.036,
-  0.016,
-  0.016,
-  0.016,
-  0.016,
-];*/
-var stretchTimeMultipliers = [1, 1, 1, 2, 2, 1, 1, 1, 2];
-var secondsPerStretch = 120;
-var stretchColours = [
-  "",
-  "lightcoral",
-  "salmon",
-  "pink",
-  "lightseagreen",
-  "aqua",
-  "limegreen",
-  "green",
-  "yellow",
-];
-var stretchEnabled = [];
+var sObj = {
+  stretchSet : {
+    CPPS: 0,
+    FAB_SEVEN: 1
+  },
+  
+  set : stretchSet.CPPS,
+  
+  
+  stretchNames : [
+    "Squat Stretch",
+    "Leg Push",
+    "Calves",
+    "Hamstring Tie",
+    "Figure Four",
+    "Abdomen",
+    "Child Pose",
+    "Butterfly",
+    "Forward Hip",
+  ],
+  currentStretch: -1, //Starts at negative to skip the first one if disabled
+  //Debug stretches
+  /*var sObj.stretchTimeMultipliers = [
+    0.016,
+    0.016,
+    0.036,
+    0.036,
+    0.016,
+    0.016,
+    0.016,
+    0.016,
+  ];*/
+  stretchTimeMultipliers : [1, 1, 1, 2, 2, 1, 1, 1, 2],
+  secondsPerStretch : 120,
+  stretchColours : [
+    "",
+    "lightcoral",
+    "salmon",
+    "pink",
+    "lightseagreen",
+    "aqua",
+    "limegreen",
+    "green",
+    "yellow",
+  ],
+  stretchEnabled : [],
+  
+}
 
 var paused = true;
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  changeStretchSet();
+  changeStretchSet(sObj);
 
-  document.getElementById("stretchName").innerHTML = stretchNames[0];
+  document.getElementById("stretchName").innerHTML = sObj.stretchNames[0];
 
   document.querySelectorAll("button").forEach((button) => {
     if (button.id == "pause")
       button.onclick = () => {
-        if (currentStretch == -1) {
+        if (sObj.currentStretch == -1) {
           //Check to hide options the first time
           let optionsMenu = document.getElementById("options");
           optionsMenu.style.display = "none";
 
           //Save a cookie of the time you set.
-          createCookie("timeForSet" + (set), secondsPerStretch, 1);
+          createCookie("timeForSet" + (sObj.set), sObj.secondsPerStretch, 1);
 
           NextStretch();
         } else TogglePause();
@@ -73,45 +90,46 @@ document.addEventListener("DOMContentLoaded", () => {
     else if (button.id == "halveTime")
       //Lower the time
       button.onclick = () => {
-        if (secondsPerStretch <= 30) secondsPerStretch = 270;
-        secondsPerStretch = secondsPerStretch - 30;
-        button.innerHTML = "Time " + secondsPerStretch.toString();
+        if (sObj.secondsPerStretch <= 30) sObj.secondsPerStretch = 270;
+        sObj.secondsPerStretch = sObj.secondsPerStretch - 30;
+        button.innerHTML = "Time " + sObj.secondsPerStretch.toString();
       };
       else if (button.id == "save")
-      button.onclick = () =>{saveStretchMultipliers(set);};
+      button.onclick = () =>{saveStretchMultipliers(sObj);};
   });
 
   //Get cloning the checkbox to allow you to enable certain stretches
   let p = document.getElementById("enableStretch");
 
-  for (let i = 0; i < stretchNames.length; i++) {
+  for (let i = 0; i < sObj.stretchNames.length; i++) {
     //Checkbox
     let newCheck = p.cloneNode(true);
-    newCheck.id = "enableStretch";
-    +i.toString();
+    newCheck.id = "enableStretch"+i.toString();
     newCheck.children[0].id = "check" + i.toString();
-    newCheck.children[1].innerHTML = stretchNames[i];
-    stretchEnabled.push(true);
+    newCheck.children[1].innerHTML = sObj.stretchNames[i];
     newCheck.children[1].for = "check1" + i.toString();
+
+    if(!sObj.stretchEnabled[i]) sObj.stretchEnabled.push(true);
+    newCheck.value=sObj.stretchEnabled[i];
 
     //Time slider
     newCheck.children[2].id = "timeSlider" + i.toString();
-    newCheck.children[2].value = stretchTimeMultipliers[i] * 2;
+    newCheck.children[2].value = sObj.stretchTimeMultipliers[i] * 2;
     newCheck.children[3].for = "timeSlider" + i.toString();
     newCheck.children[3].innerHTML =
-      stretchTimeMultipliers[i] * secondsPerStretch;
+      sObj.stretchTimeMultipliers[i] * sObj.secondsPerStretch;
 
     //Toggle the stretch here!
     newCheck.children[0].onclick = () => {
-      stretchEnabled[i] = !stretchEnabled[i];
-      console.log(stretchEnabled[i]);
+      sObj.stretchEnabled[i] = !sObj.stretchEnabled[i];
+      console.log(sObj.stretchEnabled[i]);
     };
 
     //Change stretch time multiplier here!
     newCheck.children[2].onchange = () => {
       let newVal = newCheck.children[2].value / 2;
-      stretchTimeMultipliers[i] = newVal;
-      newCheck.children[3].innerHTML = newVal * secondsPerStretch;
+      sObj.stretchTimeMultipliers[i] = newVal;
+      newCheck.children[3].innerHTML = newVal * sObj.secondsPerStretch;
     };
 
     document.getElementById("options").appendChild(newCheck);
@@ -123,12 +141,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let enableAll=true;
 
-      for (let i = 0; i < stretchNames.length; i++) {
+      for (let i = 0; i < sObj.stretchNames.length; i++) {
         if (document.getElementById("check" + i.toString()).checked) enableAll=false;
       }
 
-    for (let i = 0; i < stretchNames.length; i++) {
-      stretchEnabled[i] = enableAll;
+    for (let i = 0; i < sObj.stretchNames.length; i++) {
+      sObj.stretchEnabled[i] = enableAll;
       document.getElementById("check" + i.toString()).checked = enableAll;
     }
   };
@@ -142,8 +160,8 @@ document.addEventListener("DOMContentLoaded", () => {
   p.children[3].style.display = "none";
 
   //Set stretches based on what the cached checkboxes are
-  for (let i = 0; i < stretchNames.length; i++) {
-    stretchEnabled[i] = document.getElementById("check" + i.toString()).checked;
+  for (let i = 0; i < sObj.stretchNames.length; i++) {
+    sObj.stretchEnabled[i] = document.getElementById("check" + i.toString()).checked;
   }
 
 });
@@ -168,40 +186,40 @@ function Tick() {
       (time % 60).toString();
 
     //Vibrate phone when finished a cycle. Do it twice when over a minute.
-    if (time > 60 && time % ((secondsPerStretch*stretchTimeMultipliers[currentStretch])/2) == 0) window.navigator.vibrate(500);
-    else if (time % secondsPerStretch == 0) window.navigator.vibrate(500);
+    if (time > 60 && time % ((sObj.secondsPerStretch*sObj.stretchTimeMultipliers[sObj.currentStretch])/2) == 0) window.navigator.vibrate(500);
+    else if (time % sObj.secondsPerStretch == 0) window.navigator.vibrate(500);
     //Vibrate for quick two part stretches (i.e. vibrate per minute instead of per two minutes)
-    else if (time % (secondsPerStretch / 2) == 0 && currentStretch == 2 && set == stretchSet.CPPS)
+    else if (time % (sObj.secondsPerStretch / 2) == 0 && sObj.currentStretch == 2 && set == sObj.stretchSet.CPPS)
       window.navigator.vibrate(500);
-    else if (time % ((secondsPerStretch*stretchTimeMultipliers[currentStretch])/3) == 0 && (currentStretch == 1 || currentStretch == 2) && set == stretchSet.FAB_SEVEN)
+    else if (time % ((sObj.secondsPerStretch*sObj.stretchTimeMultipliers[sObj.currentStretch])/3) == 0 && (sObj.currentStretch == 1 || sObj.currentStretch == 2) && sObj.set == stretchSet.FAB_SEVEN)
       window.navigator.vibrate(500);
 
     //Go on to the next stretch after the set amount of time in the arrays
-    if (time >= secondsPerStretch * stretchTimeMultipliers[currentStretch])
+    if (time >= sObj.secondsPerStretch * sObj.stretchTimeMultipliers[sObj.currentStretch])
       NextStretch();
   }
 }
 
 //Go to the next stretch once time is up or during the first unpause.
 function NextStretch() {
-  currentStretch++;
+  sObj.currentStretch++;
 
   //Go to name of next stretch
   var nextName = "";
   //Or don't if complete.
-  if (currentStretch >= stretchNames.length) {
+  if (sObj.currentStretch >= sObj.stretchNames.length) {
     nextName = "Complete!";
     TogglePause();
-  } else nextName = stretchNames[currentStretch];
+  } else nextName = sObj.stretchNames[sObj.currentStretch];
   document.getElementById("stretchName").innerHTML = nextName;
 
   //Change colour to next stretch colour
-  if (currentStretch < stretchColours.length)
+  if (sObj.currentStretch < sObj.stretchColours.length)
     document.getElementById("wrapper").style.backgroundColor =
-      stretchColours[currentStretch];
+      sObj.stretchColours[sObj.currentStretch];
 
   //Skip stretch if disabled
-  if (!stretchEnabled[currentStretch]) {
+  if (!sObj.stretchEnabled[sObj.currentStretch]) {
     NextStretch();
     return;
   }
@@ -213,117 +231,4 @@ function NextStretch() {
 function TogglePause() {
   paused = !paused;
   document.getElementById("pause").innerHTML = paused ? "Unpause" : "Pause";
-}
-
-
-//Change to a different set of stretches depending on url query
-function changeStretchSet()
-{
-
-  const urlParams = new URLSearchParams(window.location.search);
-
-  set = urlParams.get('q');
-  if (set == null || set == stretchSet.CPPS)
-  {
-    stretchTimeMultipliers = [1, 1, 1, 2, 2, 1, 1, 1, 2];
-    stretchNames = [
-    "Squat Stretch",
-    "Leg Push",
-    "Calves",
-    "Hamstring Tie",
-    "Figure Four",
-    "Abdomen",
-    "Child Pose",
-    "Butterfly",
-    "Forward Hip",
-  ];
-  
-  secondsPerStretch = getCookie("timeForSet0");
-  if (secondsPerStretch == "") secondsPerStretch = 120;
-
-  }
-  else if (set == stretchSet.FAB_SEVEN)
-  {
-    stretchTimeMultipliers = [
-      2,
-      1.5,
-      1.5,
-      1,
-      1,
-      1,
-      2,
-      1,
-      1,
-    2];
-    stretchNames = [
-    "Oburator",
-    "Lunge Set 1",
-    "Lunge Set 2",
-    "Child Pose",
-    "Cat Stretch",
-    "Chest Stretch",
-    "Hamstrings",
-    "Bridging",
-    "Tabletop",
-    "Rev. Kegel"
-
-  ];
-  
-  secondsPerStretch = getCookie("timeForSet1");
-  if (secondsPerStretch == "") secondsPerStretch = 120;
-
-  }
-
-  loadStretchMultipliers(set);
-
-  document.getElementById("halveTime").innerHTML = "Time " + secondsPerStretch;
-
-}
-
-function createCookie(name, value, days) {
-  var date, expires;
-  if (days) {
-      date = new Date();
-      date.setTime(date.getTime()+(days*24*60*60*1000));
-      expires = "; expires="+date.toUTCString();
-  } else {
-      expires = "";
-  }
-  document.cookie = name+"="+value+expires+"; path=/";
-}
-
-
-
-function getCookie(cname) {
-  console.log(document.cookie);
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-
-function saveStretchMultipliers(set)
-{
-  for (let i =0; i < stretchNames.length; i++)
-  {
-    createCookie(set+"multiplier"+i, stretchTimeMultipliers[i], 30);
-  }
-}
-
-function loadStretchMultipliers(set)
-{
-  for (let i =0; i < stretchNames.length; i++)
-  {
-    stretchTimeMultipliers[i] = getCookie(set+"multiplier"+i);
-  }
 }
